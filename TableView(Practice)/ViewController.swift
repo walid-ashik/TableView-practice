7800b03ac99eeada7817ffd035292e0c6c871e93
walidaits <walid@audacityit.com>
2019-07-30 10:31:32 +0600

    initial commit

---
+++ TableView(Practice)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView(Practice)
+//
+//  Created by Walid AITS on 7/30/19.
+//  Copyright © 2019 Walid AITS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
